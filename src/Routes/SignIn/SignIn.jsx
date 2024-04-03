3bdde6a177cd61dc402e19fde847eb329b8e674c
Ode Ardika <dirfanardika@gmail.com>
2024-04-03 20:44:17 +0800

    Creted SignIn Page - Create the SignIn Page - Make the SignIn Page Responsive

--- src/Routes/SignIn/SignIn.jsx
+++ src/Routes/SignIn/SignIn.jsx
@@ -1,12 +1,27 @@
 import style from './SignIn.module.css'
 import piggy_img from '../../assets/piggy_bag.png'
+import logo from '../../../public/logo.svg'
 
 function SignIn (){
 
     return(
     <div className={style.body}>
-        <h1>Page Login is not done yet</h1>
-        <a href="./signup"><button>Signup</button></a>
+        <div className={style.sign_in}>
+            <form action="#" className={style.input_field}>
+                <div className={style.logo}>
+                    <img src={logo} alt="savker logo" className={style.logo_img} />
+                    <p className={style.logo_text}>SAVKER</p>
+                </div>
+                <h1 className={style.heading}>Wellcome Back</h1>
+                <p className={style.signup_href}>Don't have an account? <a href="./signup">sign up</a></p>
+                <input type="text" placeholder='Username or Email' />
+                <input type="password" placeholder='Password' />
+                <div className={style.button_div}>
+                    <button>Sign in</button>
+                </div>
+            </form>
+            <img src={piggy_img} alt="piggy bag image" className={style.piggy_img} />
+        </div>
     </div>
     )
 }
