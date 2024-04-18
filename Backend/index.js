6aa30c6f2dcc86d90c9b44c2b5c95daa78f4339c
Ode Ardika <dirfanardika@gmail.com>
2024-04-18 18:58:45 +0800

    feat(regis-api): Create an API to handle user registration

--- Backend/index.js
+++ Backend/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const regisUser = require('./utils/auth/registration')
 
 const app = express()
 
@@ -7,6 +8,37 @@ app.use(express.json())
 app.get('/', (req, res) => {
     res.send("Express is Running")
 })
+app.post('/api/regis', (req, res) =>{
+    const {name, email, password , confirm_password} = req.body
+
+    if (!name || !email || !password || !confirm_password){
+        return res.status(400).json({
+            error : "Missing information",
+            message : "Please provide your email, username, and password to complete the registration."
+        })
+    }
+
+    if(password === confirm_password){
+        return res.status(400).json({
+            error : "Passwords do not match",
+            message : "The password and confirm password fields must match. Please ensure that both fields contain the same value."
+        })
+    }
+
+    regisUser(name,email,password)
+    .then(data => {res.status(200).json({massage : "Succesfully create user"})})
+    .catch(error => {
+        let status = 400
+        if(error.error === "Email or username already exists"){
+            status = 409
+        }
+        
+        res.status(status).json({
+            error : error
+        })
+    })
+})
+
 
 app.listen(3000 , () => {
     console.log('http://localhost:3000/')
---
+++ Backend/utils/auth/registration.js
@@ -0,0 +1,38 @@
+const emailValidator = require("email-validator");
+const createUser = require('../database/prismaUtils')
+ 
+// validator.validate("test@email.com"); // true
+
+async function regisUser(name, email, password){
+    return new Promise((resolve, reject) => {
+        // check if email in the correct format
+        if(!emailValidator.validate(email)){
+            return reject({
+                error : "Email is not valid",
+                message : "The email you entered is not valid. Please check and try again."
+            })
+        }
+        createUser(name, email, password)
+        .then(data => {
+            resolve({
+                massage : "Succesfully create user"
+            })
+        })
+        .catch(error => {
+            if(error['code'] === 'P2002') {
+                reject({
+                    error : 'Email or username already exists',
+                    message : 'The email address or username you entered is already registered. If you already have an account, please log in. If you forgot your password, you can reset it.'
+                })
+            }
+            reject(error)
+            })
+
+    })
+}
+
+module.exports = regisUser
+
+
+
+    // var result = createUser(name, email, password).then().catch()
