03f618554436f731c49fb6359219dfdc1f97cb8e
Ode Ardika <dirfanardika@gmail.com>
2024-05-01 14:27:52 +0800

    feat(api): Adding cors middleware to give frontend access to the API

--- Backend/index.js
+++ Backend/index.js
@@ -1,41 +1,47 @@
-const express = require('express')
-const regisUser = require('./utils/auth/registration')
+const express = require('express');
+const regisUser = require('./utils/auth/registration');
+const cors = require('cors');
 
-const app = express()
 
-app.use(express.json())
+const app = express();
+
+app.use(express.json());
+
+// give access for the frontend to use API
+app.use(cors({
+    origin : 'http://localhost:5173'
+}));
 
 app.get('/', (req, res) => {
     res.send("Express is Running")
 })
-app.post('/api/regis', (req, res) =>{
-    const {name, email, password , confirm_password} = req.body
 
-    if (!name || !email || !password || !confirm_password){
+app.post('/api/regis', (req, res) =>{
+    const {email, password , confirmPassword, username} = req.body
+
+    if (!email || !password || !confirmPassword || !username){
         return res.status(400).json({
             error : "Missing information",
             message : "Please provide your email, username, and password to complete the registration."
         })
     }
 
-    if(password === confirm_password){
+    if(password !== confirmPassword){
         return res.status(400).json({
             error : "Passwords do not match",
             message : "The password and confirm password fields must match. Please ensure that both fields contain the same value."
         })
     }
 
-    regisUser(name,email,password)
+    regisUser(username,email,password)
     .then(data => {res.status(200).json({massage : "Succesfully create user"})})
     .catch(error => {
-        let status = 400
+        let status = 400;
         if(error.error === "Email or username already exists"){
-            status = 409
-        }
+            status = 409;
+        };
         
-        res.status(status).json({
-            error : error
-        })
+        res.status(status).json({error});
     })
 })
 
