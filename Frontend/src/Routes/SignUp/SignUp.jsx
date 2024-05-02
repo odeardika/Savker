8bafd1ab355650053e1cfe9488b26ac98e1eff30
Ode Ardika <dirfanardika@gmail.com>
2024-05-02 19:48:47 +0800

    feat(signup): Adding some error handling message

--- Frontend/src/Routes/SignUp/SignUp.jsx
+++ Frontend/src/Routes/SignUp/SignUp.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 import style from './SignUp.module.css';
 import piggy_img from '../../assets/piggy_bag.png';
@@ -12,12 +12,48 @@ function SignUp (){
     const [password, setPassword] = useState("");
     const [confirmPassword, setConfirmPassword] = useState("");
     const [emailError, setEmailError] = useState(false);
+    const [usernameError, setUsernameError] = useState(false);
+    const [passwordError, setPasswordError] = useState(false);
+    const [confirmPasswordError, setConfirmPasswordError] = useState(false);
+    const [errorMessage, setErrorMessage] = useState("");
     const navigate = useNavigate();
+
+    useEffect(()=> {
+        setEmailError(false);
+        setUsernameError(false);
+        setPasswordError(false);
+        setConfirmPasswordError(false);
+    }, [email, username, password,confirmPassword])
 
     const handleUsername = (event) => setUsername(event.target.value);
     const handleEmail = (event) => setEmail(event.target.value);
     const handlePassword = (event) => setPassword(event.target.value);
     const handleConfirmPassword = (event) => setConfirmPassword(event.target.value);
+
+    // handle error
+    const handleEmailOrUsernameExist = (message) => {
+        setEmailError(true);
+        setUsernameError(true);
+        return setErrorMessage(message);
+    }
+    const handleInvalidEmail = message => {
+        setEmailError(true);
+        return setErrorMessage(message);
+    }
+    const handlePasswordDontMatch = (message) => {
+        setPasswordError(true);
+        setConfirmPasswordError(true);
+        return setErrorMessage(message);
+    }
+    const handleMissingData = (message) => {
+        if(!username) setUsernameError(true);
+        if(!email) setEmailError(true);
+        if(!password) setPasswordError(true);
+        if(!confirmPassword) setConfirmPasswordError(true);
+        return setErrorMessage(message);
+    }
+
+
     const handleSubmit = (event) => {
         event.preventDefault();
         axios.post('http://localhost:3000/api/regis', {
@@ -36,9 +72,20 @@ function SignUp (){
             }
         }).catch(err => {
             if(err.response.data.error.error === "Email is not valid"){
-                alert("Email not valid")
+                return handleInvalidEmail("Email is not valid");
             }
+            if(err.response.data.error.error === "Email or username already exists"){
+                return handleEmailOrUsernameExist("Email or username already exists");
+            }
+            if( err.response.data.error === "Passwords do not match"){
+                return handlePasswordDontMatch("The password and confirm password doesn't match.")
+            }if(err.response.data.error === "Missing information"){
+                return handleMissingData("This field still empty", err.response.data.missing_data);
+            }
+            // return console.log(err)
         });
+
+        
 
     }
     return(
@@ -51,11 +98,18 @@ function SignUp (){
                 </div>
                 <h3>Get Started</h3>
                 <p className={style.text_signin}>Already have an account? <a href="./"><span>sign in</span></a></p>
-                <input value={username} onChange={handleUsername} type="text" placeholder='Username' />
-                <input value={email} onChange={handleEmail} className={(emailError)? style.email_error : ""} type="email" placeholder='Email'/>
-                <p className={(emailError)? style.error_message : style.hide_massage}>Email is not valid</p>
-                <input value={password} onChange={handlePassword} type="password" placeholder='Password' />
-                <input value={confirmPassword} onChange={handleConfirmPassword} type="password" placeholder='Confirm Password'/>
+                <input value={username} onChange={handleUsername} className={(usernameError)? style.input_error : ""} type="text" placeholder='Username' />
+                <p className={(usernameError)? style.error_message : style.hide_massage}>{(!errorMessage)? "" : errorMessage}</p>
+
+                <input value={email} onChange={handleEmail} className={(emailError)? style.input_error : ""} type="email" placeholder='Email'/>
+                <p className={(emailError)? style.error_message : style.hide_massage}>{(!errorMessage)? "" : errorMessage}</p>
+
+                <input value={password} onChange={handlePassword} className={(passwordError)? style.input_error : ""} type="password" placeholder='Password' />
+                <p className={(passwordError)? style.error_message : style.hide_massage}>{(!errorMessage)? "" : errorMessage}</p>
+                
+                <input value={confirmPassword} onChange={handleConfirmPassword} className={(confirmPasswordError)? style.input_error : ""} type="password" placeholder='Confirm Password'/>
+                <p className={(confirmPasswordError)? style.error_message : style.hide_massage}>{(!errorMessage)? "" : errorMessage}</p>
+
                 <div className={style.button_div}>
                     <button>Sign up</button>
                 </div>
